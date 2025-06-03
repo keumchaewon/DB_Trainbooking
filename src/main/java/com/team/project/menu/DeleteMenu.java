516e06ec97a957d82c7a16636e41b17c0679c594
seoyoungleeme <okcathy@ewhain.net>
2025-06-03 14:03:27 +0900

    delete menu translate

--- src/main/java/com/team/project/menu/DeleteMenu.java
+++ src/main/java/com/team/project/menu/DeleteMenu.java
@@ -9,16 +9,16 @@ import java.sql.*;
 public class DeleteMenu {
     public static void run(Scanner sc) {
         System.out.println("\n[Delete Menu]");
-        System.out.println("1. 예약 취소"); //예약 취소 + 좌석 복원
-        System.out.println("2. 회원 삭제");
-        System.out.print("선택: ");
+        System.out.println("1. Cancel Reservation"); //예약 취소 + 좌석 복원
+        System.out.println("2. Delete User");
+        System.out.print("Select option: ");
         int choice = sc.nextInt();
 
         try (Connection conn = ConnectionManager.getConnection()) {
             switch (choice) {
                 case 1 -> deleteReservation(conn, sc); // 1. 예약 취소
                 case 2 -> deleteUser(conn, sc); // 2. 사용자 삭제
-                default -> System.out.println("잘못된 선택");
+                default -> System.out.println("Invalid option.");
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -35,7 +35,7 @@ public class DeleteMenu {
                 PreparedStatement delRes = conn.prepareStatement("DELETE FROM reservation WHERE reservation_id = ?");
                 PreparedStatement freeSeat = conn.prepareStatement("UPDATE seat SET is_reserved = 0 WHERE seat_id = ?");
         ) {
-            System.out.print("삭제할 예약 ID: ");
+            System.out.print("Reservation ID for deletion: ");
             int resId = sc.nextInt();
 
             getSeat.setInt(1, resId); //좌석 찾기
@@ -50,14 +50,14 @@ public class DeleteMenu {
                 freeSeat.executeUpdate();
 
                 conn.commit();
-                System.out.println("예약 삭제 완료");
+                System.out.println("Reservation deleted successfully");
             } else {
                 conn.rollback();
-                System.out.println("예약 정보 없음");
+                System.out.println("Reservation not found");
             }
         } catch (Exception e) {
             conn.rollback();
-            System.out.println("트랜잭션 실패");
+            System.out.println("Transaction failed");
         } finally {
             conn.setAutoCommit(true);
         }
